695cde063432ed73de0c015fcd2f6e6ab59fd0b3
MartialMAY <mayamoumartial@gmail.com>
2023-10-16 11:41:24 +0200

    update

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>App RappelTout</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="form1">
+
+        </div>
+        <div class="form2-3">
+            <div class="form2">
+                <ul>
+                    <li id="Dashboard"><h2>Dashboard</h2></li>
+                    <li id="Materiel"><h2>Materiel</h2></li>
+                    <li id="Document"><h2>Document</h2></li>
+                </ul>
+                
+
+            </div>
+            <div class="form3">
+                <div class="formulaire">
+                    <h1>Ajouter un materiel</h1>
+                    <form action="">
+
+                    </form>
+                </div>
+                
+            </div>
+        </div>
+        <div class="form4">
+    </div>
+    <ul>
+
+    </ul>
+</body>
+</html>
